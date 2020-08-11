3cd2d4029fefa5405695cf41da41d119ebdc9477
adamcpenman <adamcpenman@gmail.com>
2020-08-11 17:57:39 -0500

    room class created

---
+++ src2/room.py
@@ -0,0 +1,25 @@
+# Implement a class to hold room information. This should have name and
+# description attributes.
+
+class Room:
+    def __init__(self, name, description):
+        # attributes
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.w_to = None
+        self.s_to = None
+        self.e_to = None
+
+    def __str__(self):
+        output = f'"ROOM"{self.name}: {self.description}\n'
+        if self.n_to:
+            output += 'To the north is: ' + self.n_to.name + '\n'
+        if self.e_to:
+            output += 'To the east is: ' + self.e_to.name + '\n'
+        if self.s_to:
+            output += 'To the south is: ' + self.s_to.name + '\n'
+        if self.w_to:
+            output += 'To the west is: ' + self.w_to.name + '\n'
+
+        return output
